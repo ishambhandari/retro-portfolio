931387772894850c2a3888bdcc7a0430bbb9e59a
isham bhandari <ishambhandari007@gmail.com>
2021-09-05 00:00:51 +0545

    very minor changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,38 +1,34 @@
-import React from "react";
-import hamburger from "../../Assets/hamburger.png";
-import { ModalContext } from "../../Context/ModalState";
-import Modal from "../Modal/Modal";
-import "./nav.css";
+import React from 'react';
+import hamburger from '../../Assets/hamburger.png';
+import {ModalContext} from '../../Context/ModalState';
+import Modal from '../Modal/Modal';
+import './nav.css';
 
 const Navbar = () => {
-    const [showModal, setShowModal] = React.useContext(ModalContext);
-    const onClickModal = () => {
-        setShowModal(true);
-    };
-    return (
-        <div>
-            <div className="nav-elements">
-                <ul className="ul-list">
-                    <a>
-                        <li className="link">Home</li>
-                    </a>
-                    <li>About</li>
-                    <li>Works</li>
-                    <li>Contact</li>
-                </ul>
-                <div className="hamburger">
-                    <button
-                        type="button"
-                        class="nes-btn"
-                        onClick={onClickModal}
-                    >
-                        <img src={hamburger} className="img-ham" />
-                    </button>
+  const [showModal, setShowModal] = React.useContext(ModalContext);
+  const onClickModal = () => {
+    setShowModal(true);
+  };
+  return (
+    <div>
+      <div className="nav-elements">
+        <ul className="ul-list">
+          <a href="#work-page">
+            <li className="link">Home</li>
+          </a>
+          <li>About</li>
+          <li>Works</li>
+          <li>Contact</li>
+        </ul>
+        <div className="hamburger">
+          <button type="button" class="nes-btn" onClick={onClickModal}>
+            <img src={hamburger} className="img-ham" />
+          </button>
 
-                    {showModal && <Modal />}
-                </div>
-            </div>
+          {showModal && <Modal />}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 export default Navbar;
