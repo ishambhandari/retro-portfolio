74a5d8deac30d84e86d0b8659416bf570f9e725b
isham bhandari <ishambhandari007@gmail.com>
2021-11-24 15:33:21 +0545

    image slider modal

--- src/pages/Works/Works.js
+++ src/pages/Works/Works.js
@@ -3,17 +3,29 @@ import Line from "../../components/Line/Line";
 import Modal from "../../components/Modal/Modal";
 import { ModalContext } from "../../Context/ModalState";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import images123 from "./testImages.js";
 import "./works.css";
 import project1 from "../../Assets/project1.png";
 import project2 from "../../Assets/project2.png";
 import project3 from "../../Assets/project3.png";
 const Works = () => {
   const [showModal, setShowModal] = React.useContext(ModalContext);
+  const [current, setCurrent] = React.useState(0);
   const worksRef = React.useRef(null);
   const onClickModal = () => {
     setShowModal(true);
   };
-  React.useEffect(() => {}, [showModal]);
+  React.useEffect(() => {
+    console.log("images", images123);
+  }, [showModal]);
+
+  const nextSlide = () => {
+    setCurrent(current === images123.length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? images123.length - 1 : current - 1);
+  };
   return (
     <Router>
       <div id="workPage">
@@ -47,7 +59,39 @@ const Works = () => {
             </button>
           </div>
         </div>
-        {showModal && <Modal></Modal>}
+        {showModal && (
+          <Modal>
+            {/* <img */}
+            {/*   src="https://images.saymedia-content.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDY0NTMyOTQzNDgwNDU0/buying-your-first-desktop-computer.jpg" */}
+            {/*   alt="asdf" */}
+            {/* /> */}
+
+            <div className="image-modal-container">
+              <button
+                className="nes-btn slider-btn-left"
+                type="button"
+                onClick={prevSlide}
+              >
+                &lt;
+              </button>
+
+              <button
+                className="nes-btn slider-btn-right"
+                type="button"
+                onClick={nextSlide}
+              >
+                &gt;
+              </button>
+              {images123.map((res, index) => {
+                return (
+                  <div className={index === current ? "slide-active" : "slide"}>
+                    <img src={res.url} alt="photo" className="modal-image" />
+                  </div>
+                );
+              })}
+            </div>
+          </Modal>
+        )}
         <Line />
       </div>
     </Router>
---
+++ src/pages/Works/testImages.js
@@ -0,0 +1,15 @@
+const images123 = [
+  {
+    url: "https://www.looper.com/img/gallery/the-best-anime-to-watch-if-youre-an-anime-newbie/intro-1633148302.jpg",
+  },
+  {
+    url: "https://images.saymedia-content.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDY0NTMyOTQzNDgwNDU0/buying-your-first-desktop-computer.jpg",
+  },
+  {
+    url: "https://images.saymedia-content.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDY0NTMyOTQzNDgwNDU0/buying-your-first-desktop-computer.jpg",
+  },
+  {
+    url: "https://images.saymedia-content.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc0NDY0NTMyOTQzNDgwNDU0/buying-your-first-desktop-computer.jpg",
+  },
+];
+export default images123;
