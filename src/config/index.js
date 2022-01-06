51783b3676f3c199e53dc239cb06746cf6ad62ec
isham bhandari <ishambhandari007@gmail.com>
2022-01-06 14:33:39 +0545

    index.html changes

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@
 
 const envConfig = {
   // BASEURL: "http:localhost:5000",
-  BASEURL: "https:stark-reaches-31075.herokuapp.com",
+  BASEURL: "https://stark-reaches-31075.herokuapp.com",
 };
 
 export { envConfig };
